859b822b0486cce2a8ef5e53c933ffdc7787ef00
Rehan Sajid <rehansajid.dev@gmail.com>
2016-09-23 15:09:10 +0500

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Rehan Sajid on 23/09/2016.
+//  Copyright © 2016 Rehan Sajid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
